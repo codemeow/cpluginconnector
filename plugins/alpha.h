29ae1a3a0ef44fb6ecd791c316ef158af6cde1cf
codemeow <alexcones@gmail.com>
2015-01-30 14:24:44 +0400

    Update alpha.h

--- plugins/alpha.h
+++ plugins/alpha.h
@@ -1,6 +1,11 @@
+#ifndef ALPHA_H
+#define ALPHA_H
+
 #include "plugtypes.h"
 
 void initalpha();
 
 errorcode AlphaHeader(DataStorage *data);
 errorcode AlphaValues(Hash value,  DataStorage *data);
+
+#endif // ALPHA_H
